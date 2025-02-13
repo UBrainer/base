1e56be7d56952307d312b2df697058810c599d4c
UBrainer <brainerg@uninorte.edu.co>
2025-02-13 17:38:38 -0500

    Update run_bechmark.sh

--- run_bechmark.sh
+++ run_bechmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Clonar el repositorio con las soluciones
-git clone https://github.com/UBrainer/act2.git solutions
-cd solutions
+git clone https://github.com/UBrainer/act2.git
+cd act2
 
 # Ejecutar los contenedores
 docker-compose up --build --abort-on-container-exit
@@ -10,8 +10,8 @@ docker-compose up --build --abort-on-container-exit
 # Capturar resultados y mostrarlos
 echo "Resultados del Benchmark:"
 echo "--------------------------------------"
-for lang in Python Node Java C++ Rust; do
-    output_file="solutions/$lang/output.txt"
+for lang in Python Cpp node java rust; do
+    output_file="$lang/output.txt"
     if [ -f "$output_file" ]; then
         time_ms=$(cat "$output_file")
         echo "$lang: ${time_ms}ms"
@@ -19,3 +19,4 @@ for lang in Python Node Java C++ Rust; do
         echo "$lang: No se encontró output.txt"
     fi
 done
+
