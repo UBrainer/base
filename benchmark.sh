8042230844fb6b9212a151e95f7b751cfad31418
UBrainer <brainerg@uninorte.edu.co>
2025-02-13 16:18:19 -0500

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
-# Lista de lenguajes y sus imágenes
-declare -A languages=(
-    ["Python"]="solucion-python"
-    ["Java"]="solucion-java"
-    ["C++"]="solucion-cpp"
-    ["Node.js"]="solucion-node"
-    ["Rust"]="solucion-rust"
-)
+# Clonar el repositorio con las soluciones
+git clone https://github.com/UBrainer/act2.git
+cd act2 || exit 1
 
-echo "Lenguaje | Tiempo (ms)"
-echo "----------------------"
+# Lista de lenguajes y sus carpetas
+LANGUAGES="Python C++ Node Java Rust"
+OUTPUT_FILE="resultados.txt"
 
-# Recorrer cada contenedor y ejecutar
-for lang in "${!languages[@]}"; do
-    time_output=$(docker run --rm "${languages[$lang]}")  # Ejecuta el contenedor y captura el tiempo
-    echo "$lang | $time_output"
+# Limpiar archivo de resultados si existe
+echo "Lenguaje Tiempo(ms)" > $OUTPUT_FILE
+
+# Recorrer cada lenguaje, construir y ejecutar su contenedor
+for lang in $LANGUAGES; do
+    IMAGE_NAME="solucion$(echo $lang | tr '[:upper:]' '[:lower:]')"
+
+    # Construcción de la imagen
+    docker build -t $IMAGE_NAME ./$lang
+
+    # Ejecución del contenedor y captura de la salida (tiempo de ejecución)
+    TIME=$(docker run --rm $IMAGE_NAME 2>/dev/null)
+
+    # Guardar el resultado en el archivo
+    echo "$lang $TIME" >> $OUTPUT_FILE
 done
+
+# Mostrar los resultados en formato tabla
+echo "Resultados del benchmark:"
+column -t $OUTPUT_FILE
