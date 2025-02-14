225ebab79c51a8c89a9caa784fe533233d5a8feb
UBrainer <brainerg@uninorte.edu.co>
2025-02-14 07:10:51 -0500

    c toC

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -10,7 +10,7 @@ echo "Lenguaje | Tiempo (ms)"
 echo "----------------------"
 
 # Recorrer cada carpeta y ejecutar su contenedor
-for lang in cpp python node java rust; do
+for lang in Cpp python node java rust; do
     cd $lang
     docker build -t benchmark-$lang . > /dev/null 2>&1
     result=$(docker run --rm benchmark-$lang)
