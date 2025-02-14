f466fd683c90119711b3e0fce1b24b094cd2c8d4
UBrainer <brainerg@uninorte.edu.co>
2025-02-14 07:06:01 -0500

    bench segundo

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,30 +1,22 @@
-#!/bin/bash
-set -e  # Detener el script en caso de error
+#!/bin/sh
+set -e
 
 # Clonar el repositorio con las soluciones
-git clone https://github.com/UBrainer/act2.git /benchmark
+git clone https://github.com/UBrainer/act2.git
+cd act2
 
-# Definir la cabecera de la tabla
-echo -e "Lenguaje\tTiempo (ms)" > resultados.txt
+# Encabezado de la tabla
+echo "Lenguaje | Tiempo (ms)"
+echo "----------------------"
 
-# Recorrer las carpetas de los lenguajes
+# Recorrer cada carpeta y ejecutar su contenedor
 for lang in cpp python node java rust; do
-    echo "Ejecutando $lang..."
-    
-    # Construir la imagen del contenedor
-    docker build -t benchmark-$lang /benchmark/$lang
-    
-    # Ejecutar el contenedor y capturar la salida en output.txt
-    docker run --rm benchmark-$lang > /benchmark/$lang/output.txt
-    
-    # Leer el tiempo de ejecución
-    time_ms=$(cat /benchmark/$lang/output.txt)
-    
-    # Agregar el resultado a la tabla
-    echo -e "$lang\t$time_ms" >> resultados.txt
+    cd $lang
+    docker build -t benchmark-$lang . > /dev/null 2>&1
+    result=$(docker run --rm benchmark-$lang)
+    echo "$lang | $result"
+    cd ..
 done
 
-# Mostrar la tabla final
-cat resultados.txt
 
 
